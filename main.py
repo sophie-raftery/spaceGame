0d13fc6ffa11e4c1a690ee98a7a287c7da05b35f
sophie-raftery <sophie.raftery@ericsson.com>
2026-06-09 13:04:40 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #Space Game
 import pygame
+from tkinter import font
 from random import randint, uniform
 from os.path import join, dirname, abspath
 from operator import pos
 
+#player class
 class Player(pygame.sprite.Sprite):
     def __init__(self, groups):
         super().__init__(groups)
@@ -60,6 +62,7 @@ class Laser(pygame.sprite.Sprite):
         if self.rect.bottom < 0:
             self.kill()
 
+#meteor class
 class Meteor(pygame.sprite.Sprite):
     def __init__(self, surf, pos, groups):
         super().__init__(groups)
@@ -76,6 +79,7 @@ class Meteor(pygame.sprite.Sprite):
         if pygame.time.get_ticks() - self.start_time > self.life_time:
             self.kill()
 
+#collision function
 def collisions():
     global running
 
@@ -87,6 +91,14 @@ def collisions():
         collided_sprites = pygame.sprite.spritecollide(laser, meteor_sprites, True)
         if collided_sprites:
             laser.kill()
+
+#display score function
+def display_score():
+    current_time = pygame.time.get_ticks() // 100
+    text_surf = font.render(str(current_time), True, (240, 240, 240))
+    text_rect = text_surf.get_frect(midbottom = (WINDOW_WIDTH / 2, WINDOW_HEIGHT - 50))
+    display_surface.blit(text_surf, text_rect)
+    pygame.draw.rect(display_surface, (240, 240, 240), text_rect.inflate(20,10).move(0, -8), 5, 10)
 
 #general setup
 pygame.init()
@@ -116,7 +128,8 @@ player = Player(all_sprites)
 star_surf = pygame.image.load(join("spacegame/images/star.png")).convert_alpha()
 meteor_surf = pygame.image.load(join("spacegame/images/meteor.png")).convert_alpha()
 laser_surf = pygame.image.load(join("spacegame/images/laser.png")).convert_alpha()
-
+font = pygame.font.Font(join("spacegame/images/Oxanium-Bold.ttf"), 20)
+text_surf = font.render("text", True, (240, 240, 240))
 
 # player_surf = pygame.image.load(join("spacegame", "images", "player.png")).convert_alpha()
 # player_rect = player_surf.get_frect(center=(WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
@@ -138,9 +151,10 @@ while running:
             x, y = randint(0, WINDOW_WIDTH), randint(-200, -100)
             Meteor(meteor_surf, (x, y), (all_sprites, meteor_sprites))
 
-    #draw the game
+    #draw the game, whatever is drawn last sits on top
     display_surface.fill("black") #background colour
     all_sprites.draw(display_surface)
+    display_score()
 
     all_sprites.update(dt)
     collisions()
